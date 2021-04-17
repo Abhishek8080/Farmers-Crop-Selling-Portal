1f34a43580a0b8703f485f1a1ac70c5606fae3ed
Abhishek Tripathi <51128875+Abhishek8080@users.noreply.github.com>
2021-04-17 13:49:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://Abhishek:Abhishek@07@cluster0.mxcl2.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": ">V]77EWp.U$9$gQeZ%qma])M-5/7nsMVw5,Z<e$pGk:Y]sV!5$",
-        "REFRESH_TOKEN_SECRET": "J;wD<tCYe+9.GT%;$$/SxA3-(C,~(9h/D8D3~%8)jA:/^\%S$B:EBu.SW$tmYejDLsA^+6\y59Pwm+U",
-        "PAYPAL_CLIENT_ID": "AZLFGrO-_N_-eMJZdqULyD_mNuN4zSic4jGVBaAjCtQotBZtc6i8Zg4TH9VuRcaLsAPUY-0yWK05ELg4",
+        "MONGODB_URL": "mongodb+srv://Abhishek:password@cluster0.mxcl2.mongodb.net/database_name?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "",
+        "REFRESH_TOKEN_SECRET": "",
+        "PAYPAL_CLIENT_ID": "",
         "CLOUD_UPDATE_PRESET": "nextjs_store",
         "CLOUD_NAME": "donrdvxt8",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/donrdvxt8/image/upload"
+        "CLOUD_API": ""
     }
 }
